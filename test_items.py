f9e07a1ba80242014e654b868d14b4c3cede2b4d
krater43 <85675961+krater43@users.noreply.github.com>
2021-06-14 14:29:15 +0300

    Stepik autotests courses
    Stepik autotests courses

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+	
+		
+def test_find_button(browser):
+    browser.get(link)
+    button = len(browser.find_elements_by_css_selector('.btn-add-to-basket'))
+    assert button > 0, 'Элемент не найдей'
