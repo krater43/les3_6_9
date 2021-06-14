98afa6bf8e499805985f704d602daa268f1fa298
krater43 <85675961+krater43@users.noreply.github.com>
2021-06-14 14:30:55 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -8,5 +8,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 		
 def test_find_button(browser):
     browser.get(link)
+    time.sleep (30)
     button = len(browser.find_elements_by_css_selector('.btn-add-to-basket'))
     assert button > 0, 'Элемент не найдей'
